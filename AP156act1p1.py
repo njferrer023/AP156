8ea6fca845cbd1073a329dcca7263d7df7ecfd2a
Nica Jane Ferrer <nicajaneferrer@yahoo.com>
2016-08-29 22:33:37 +0800

    added the .ipynb file of activity 1

---
+++ AP156act1p1.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+########################### AP156 Activity 1- Nica Jane Ferrer (2013-25765)####################################
+
+### Questions:
+#### How should the code be changed if I want the Greek symbol \pi instead?
+# Just change Pi to $\pi$:
+# plt.xticks([0, 0.25, 0.5, 0.75, 1], ["0", "$\pi$/4", "$\pi$/2", "3$\pi$/4", "$\pi$"])
+# plt.yticks([0, 2, 4, 6, 8, 10], ["0", "2$\pi$", "4$\pi$", "6$\pi$", "8$\pi$", "10$\pi$"])
+
+###### What other plot options for plt.xlabel are available?
+# 
+
+#### What would happen if plt.show() is not commented out? Try it.
+# If plt.show() is not commented out, the output figures would appear one at a time. Otherwise, all of the figures appear simultaneously.
+
+
+
+#### Double-check the expressions sinuspsi and phinorm
+# These expressions are consistent with eq 1.2 and 1.5 respectively
+
+#### Where is the zero reference point of phi?
+# The zero reference point of phi is the vertical line
+
+#### When phi/phi0 = 1, where is the bob?
+# When phi/phi0 = 1, the bob is at its maximum angular displacement
+
